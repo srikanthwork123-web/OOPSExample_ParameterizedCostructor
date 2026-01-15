5d2030b07230d9351c2a5cd04122baf8a7f14525
Dell <Dell@DESKTOP-13B42NJ>
2026-01-15 08:11:08 +0530

    Add project files.

---
+++ OOPSExample_ParameterizedCostructor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace OOPSExample_ParameterizedCostructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//For parameterized constructor you must pass the values from object creation time.otherwise it will throw error ata compiletime like please pass the values to paramerterized constructor.
+            employee emp1 = new employee(121, "rama", 1000);//data assigned to parameterized constructor.
+            emp1.displayempinfo();
+
+            Console.WriteLine("emp no is:" + emp1.eno);
+            Console.WriteLine("emp name is:" + emp1.ename);
+            Console.WriteLine("salry is:" + emp1.esal);
+            int objectcodeemp1= emp1.GetHashCode();//for every object new hashcode will geneate
+            Console.WriteLine("object hashcode address is"+objectcodeemp1);
+            employee emp2 = new employee(122, "raju", 2000);
+            emp2.displayempinfo();
+            int objectcodeemp2 = emp2.GetHashCode();//for every object new hashcode will geneate
+            Console.WriteLine("object hashcode address is" + objectcodeemp2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOPSExample_ParameterizedCostructor/employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_ParameterizedCostructor
+{
+    internal class employee
+    {//TO assign the data to instance variables we are creating the parameterized constructor and assign the data.
+      public  int eno;
+        public string ename;
+        public int esal;
+//Synatx:accessmodier classname(datatype variablename,datatype variablename,........)
+        public employee(int empno, string empname, int empsal)//parameterized constructor
+        {//constructor name and class should be same.
+            eno = empno;
+            ename = empname;
+            esal = empsal;
+          
+        }
+        public void displayempinfo()
+        {
+            Console.WriteLine("emp no is:" + eno);
+            Console.WriteLine("emp name is:" + ename);
+            Console.WriteLine("salry is:" + esal);
+        }
+    }
+}
